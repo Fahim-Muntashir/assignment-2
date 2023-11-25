dfb3ca38efdb4a486230d0542c01b1f5ea44cc21
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-11-25 17:10:05 +0600

    added finally

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -16,6 +16,112 @@ const createUser = async(req:Request, res:Response) => {
    }
 }
 
+
+const getAllUser = async (req: Request, res: Response) => {
+    try {
+        const result = await UserServices.getAllUserFromDB();
+    
+        res.status(200).json({
+            success: true,
+            message: "Users are Here",
+            data:result,
+        })
+
+    } catch (err){
+     console.log(err);   
+    }
+}
+
+const getSingleUser = async (req: Request, res: Response) => {
+    try {
+
+        const { userId } = req.params;
+        const userIdNum = parseInt(userId);
+        console.log(typeof userIdNum)
+        const result = await UserServices.getSingleUserFromDB(userIdNum);
+    
+        res.status(200).json({
+            success: true,
+            message: "User Sigle Man Here",
+            data:result,
+        })
+
+    } catch (err){
+     console.log(err);   
+    }
+}
+
+
+const updateUser = async (req: Request, res: Response) => {
+    try {
+
+        const { userId } = req.params;
+        const userIdNum = parseInt(userId);
+
+        const { user: updatedUserData } = req.body;
+
+        const existingUser = await UserServices.getSingleUserFromDB(userIdNum);
+
+        if(!existingUser){
+            res.status(404).json({
+                success: false,
+                message: "User not found",
+                data:null
+            })
+            return
+        }
+
+        const result = await UserServices.updateUserInDB(userIdNum,updatedUserData);
+    
+        const { password, ...updatedUserWithoutPassword } = result?.toObject() || {};
+        res.status(200).json({
+            success: true,
+            message: "User updated",
+            data:updatedUserWithoutPassword,
+        })
+
+    } catch (err){
+     console.log(err);   
+    }
+}
+
+const deleteUser = async (req: Request, res: Response) => {
+    try {
+        const { userId } = req.params;
+        const userIdNum = parseInt(userId);
+
+
+
+        // if the user exists 
+        const existingUser = await UserServices.getSingleUserFromDB(userIdNum);
+
+        if (!existingUser) {
+            res.status(404).json({
+                success: false,
+                message: "This User not Found",
+                data:null,
+            })
+        }
+
+
+        const result = await UserServices.deleteUserFromDB(userIdNum);
+
+        res.status(200).json({
+            success: true,
+            message: "user is deleted successfully",
+            data:result,
+        })
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 export const UserControllers = {
     createUser,
+    getAllUser,
+    getSingleUser,
+    updateUser,
+ deleteUser,
 } 
+
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -6,6 +6,31 @@ const createUser = async(user: User) => {
     return result;
 }
 
+const getAllUserFromDB = async () => {
+    const result = await UserModel.find({},{ password: 0 })
+    return result;
+}
+
+const getSingleUserFromDB = async (id:number) => {
+    const result = await UserModel.findOne({ userId: id }, { password: 0 })
+    return result;
+}
+
+const updateUserInDB = async (id: number, updatedUserData: User) => {
+
+    const result = await UserModel.findOneAndUpdate({ userId: id }, updatedUserData, { new: true });
+    return result;
+}
+
+const deleteUserFromDB = async (id: number) => {
+    const result = await UserModel.findOneAndDelete({ userId: id })
+    return result;
+}
+
 export const UserServices = {
-    createUser
+    createUser,
+    getAllUserFromDB,
+    getSingleUserFromDB,
+    updateUserInDB,
+    deleteUserFromDB,
 }
